eb1c284ef2c87e93890b94610c511d75e0d3086b
Ceyhun Kıvanç Demir <73711350+KivancD@users.noreply.github.com>
2023-04-25 12:47:48 +0300

    Update Encryption_Decryption.c

--- Encryption_Decryption.c
+++ Encryption_Decryption.c
@@ -18,21 +18,21 @@ int main()
     if(choice==1){
         printf("\n\nEnter file name to be encrypted: ");
         scanf("%21s", filename);
-        original_file=fopen(filename, "r");
-        if (original_file==NULL){
+        original_file=fopen(filename, "r");                     // firstly program opens the filename with read-only priv. 
+        if (original_file==NULL){                               // if filename can't be found return 0
             printf("Couldn't find the file, quiting...\n\n");
             return 0;
         }
-        encrypted_file = fopen("encrypted_file.txt", "w");
+        encrypted_file = fopen("encrypted_file.txt", "w");     //create a new file with write priv.
         if(encrypted_file == NULL){
             printf("Couldn't create the encrypted_file file, quiting...\n\n");
             return 0;
         }
-        string = fgetc(original_file);
-        while(string != EOF){
-            string = string+100;
-            fputc(string, encrypted_file);
-            string = fgetc(original_file);
+        string = fgetc(original_file);     //get the current strings from original file 
+        while(string != EOF){              //with while loop you are wandering around every string until end of file
+            string = string+123;           //adding +123 to every string in file causes them to change their HEX
+            fputc(string, encrypted_file);  //we put encrypted string inside the new encrypted file
+            string = fgetc(original_file);  //we get the next string in original file and this loop continues untill we reach to End Of File
         }
         printf("\nEncrypted Successfully.\n\n");
     }
@@ -54,7 +54,7 @@ int main()
         string2 = fgetc(original_file2);
         while(string2 != EOF)
         {
-            string2 = string2-100;
+            string2 = string2-123;
             fputc(string2, decrypted_file);
             string2 = fgetc(original_file2);
         }
